7bf4eb2040996828f739f03ca96c593c29bd8006
Vladimir Pietro Cravero <vladimircravero@gmail.com>
2013-05-27 23:04:45 +0200

    Added resistor rotation support and correct orientation parsing.

--- devices.py
+++ devices.py
@@ -54,10 +54,29 @@ class Component:
     def __init__(self, text_block):
         self.block = text_block
         self.dict = {}
+        print self.block
     def to_tek(self):
         if self.dict["name"] == const.__RESISTOR__:
-            # We must draw a resistor! I'm assuming it is horizontal now.
-            return "({0},{1}) to [R, l=${2}$] ({3},{4})\n".format(self.dict["x"]-0.5, self.dict["y"], self.dict["reference"], self.dict["x"]+0.5, self.dict["y"])
+            # We must draw a resistor. Let's generate the start and end coordinates based on the orientation.
+            x_start = x_end = self.dict["x"]
+            y_start = y_end = self.dict["y"]
+            if self.dict["o_01"] == -1:
+                # left to right
+                x_start -= 0.5
+                x_end += 0.5
+            elif self.dict["o_01"] == 1:
+                # right to left
+                x_start += 0.5
+                x_end -= 0.5
+            elif self.dict["o_00"] == -1:
+                # down to up
+                y_start -= 0.5
+                y_end += 0.5
+            elif self.dict["o_00"] == 1:
+                # up to down
+                y_start += 0.5
+                y_end -= 0.5
+            return "({0},{1}) to [R, l=${2}$] ({3},{4})\n".format(x_start, y_start, self.dict["reference"], x_end, y_end)
         else:
             return "%Component not supported. Sorry!\n"
             
@@ -66,30 +85,36 @@ class Component:
         for line in block_iterator:
             while const.__END_COMPONENT__ not in line:
                 line = block_iterator.next()
-                print line
-                if line.startswith(const.__COMP_LABEL__,0,1):
+                # print line
+                if line.startswith(const.__COMP_LABEL__,0,len(const.__COMP_LABEL__)):
                     temp = line.strip().split(" ")
                     temp.pop(0)
                     self.dict["name"] = temp.pop(0)
                     self.dict["reference"] = temp.pop(0)
-                elif line.startswith(const.__COMP_TIME__,0,1):
+                elif line.startswith(const.__COMP_TIME__,0,len(const.__COMP_TIME__)):
                     temp = line.strip().split(" ")
                     temp.pop(0)
                     temp.pop(0)
                     temp.pop(0)  # Don't really know what the last two values are for
                     self.dict["time_stamp"] = temp.pop(0)
-                elif line.startswith(const.__COMP_POS__,0,1):
+                elif line.startswith(const.__COMP_POS__,0,len(const.__COMP_POS__)):
                     temp = line.strip().split(" ")
                     temp.pop(0)
                     self.dict["x"] = int(temp.pop(0)) * const.__MILS_TO_CM__
                     self.dict["y"] = -int(temp.pop(0)) * const.__MILS_TO_CM__
-                elif line.startswith(const.__COMP_FIELD__,0,1):
+                elif line.startswith(const.__COMP_FIELD__,0,len(const.__COMP_FIELD__)):
                     # Need to think about this
                     do_something=1
-                elif line.startswith(const.__COMP_ALT_POS__,0,1):
-                    # This should never be used because it is discontinued
-                    do_something=1
+                elif line.startswith(const.__COMP_ENDING__,0,len(const.__COMP_ENDING__)):
+                    # This line can be either the discontinued position line or the orientation matrix line
+                    temp = line.strip().split()
+                    if len(temp) == 4:
+                        # That's an orientation matrix line!
+                        self.dict["o_00"] = int(temp.pop(0))
+                        self.dict["o_10"] = int(temp.pop(0))
+                        self.dict["o_01"] = int(temp.pop(0))
+                        self.dict["o_11"] = int(temp.pop(0))
                 else:
-                    # Should really check this out
+                    # We should never fall down here
                     do_something=1
 

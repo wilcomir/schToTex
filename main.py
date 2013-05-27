a7fcb3491a14df2a840cae04fbee75f3d1e6abc9
Vladimir Cravero <vladimircravero@gmail.com>
2013-05-27 02:43:04 +0200

    Added the const prefix and the proper Component calls

--- main.py
+++ main.py
@@ -6,42 +6,28 @@
 # This file contains all the supported devices classes
 from devices import *
 
-# These are the files that will be processed
-__INPUT_FILE_NAME__ = "kicad/test.sch"
-__OUT_FILE_NAME__ = "out.tek"
-
-# Header and footer are necessary to generate a ready-to-compile tek file
-__TEK_HEADER_FILE_NAME__ = "header.txt"
-__TEK_FOOTER__ = "; \\end{circuitikz}\n\n\\end{document}"
-
-# These strings will be used in the parser. Informations on the .sch file can be found here:
-# http://en.wikibooks.org/wiki/Kicad/file_formats#Schematic_Files_Format
-__WIRE__ = "Wire Wire Line"
-__COMPONENT__ = "$Comp"
-__END_COMPONENT__ = "$EndComp"
-__JUNCTION__ = "Connection ~"
-__NO_CONNECT__ = "NoConn ~"
-        
+# This file contains all the constants needed around
+import const
         
 """Begin of main program"""
         
 # Opening the files:
 try:
-    f_in = open(__INPUT_FILE_NAME__, "r")
+    f_in = open(const.__INPUT_FILE_NAME__, "r")
 except IOError as e:
     print "IO Error opening the input file({0}): {1}\n".format(e.errno, e.strerror)
 except:
     print "Unexpected error({0}): {1}\n", sys.exc_info()[0]
 
 try:
-    f_out = open(__OUT_FILE_NAME__, "w")
+    f_out = open(const.__OUT_FILE_NAME__, "w")
 except IOError as e:
     print "IO Error opening the output file({0}): {1}\n".format(e.errno, e.strerror)
 except:
     print "Unexpected error({0}): {1}\n", sys.exc_info()[0]
     
 try:
-    f_header = open(__TEK_HEADER_FILE_NAME__, "r")
+    f_header = open(const.__TEK_HEADER_FILE_NAME__, "r")
 except IOError as e:
     print "IO Error opening the input file({0}): {1}\n".format(e.errno, e.strerror)
 except:
@@ -57,7 +43,7 @@ f_out.close()
 
 # We need to open f_out in append mode
 try:
-    f_out = open(__OUT_FILE_NAME__, "a")
+    f_out = open(const.__OUT_FILE_NAME__, "a")
 except IOError as e:
     print "IO Error opening the output file({0}): {1}\n".format(e.errno, e.strerror)
 except:
@@ -66,22 +52,31 @@ except:
 # Parser
 
 for line in f_in:
-    if __WIRE__ in line:
+    if const.__WIRE__ in line:
         print "Wire found!"
         line = f_in.next()
         f_out.write(Wire(line).to_tek())
-    elif __JUNCTION__ in line:
+    elif const.__JUNCTION__ in line:
         print "Junction found!"
         f_out.write(Junction(line).to_tek())
-    elif __NO_CONNECT__ in line:
+    elif const.__NO_CONNECT__ in line:
         print "No connect found!"
         f_out.write(NoConnect(line).to_tek())
-    elif __COMPONENT__ in line:
+    elif const.__COMPONENT__ in line:
+        # Now that's a challenge
         print "Component found!"
+        component_block = ""
+        while const.__END_COMPONENT__ not in line:
+            component_block = component_block + line
+            line = f_in.next()
+        component_block = component_block + line
+        c = Component(component_block)
+        c.parse()
+        f_out.write(c.to_tek())
     
             
 #appending the footer            
-f_out.write(__TEK_FOOTER__)
+f_out.write(const.__TEK_FOOTER__)
 
 # Don't forget to close all the files  
 f_in.close()

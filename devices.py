c1e8ca1b88b417c29d13d18da42a50b78e8a2762
Vladimir Pietro Cravero <vladimircravero@gmail.com>
2013-06-01 21:07:56 +0200

    Changed the files to use the new const names.

--- devices.py
+++ devices.py
@@ -1,10 +1,12 @@
-import const
+#!/usr/bin/env python
+
+from const import *
 
 # Simple wire connection
 class Wire:
     def __init__(self, line):
         coords = Wire.parse(line)
-        self.c = [const.__MILS_TO_CM__ * x for x in coords]
+        self.c = [MILS_TO_CM * x for x in coords]
    
     def to_tek(self):
         return "({0},{1}) to ({2},{3})\n".format(self.c[0],-self.c[1],self.c[2],-self.c[3])
@@ -18,7 +20,7 @@ class Wire:
 class Junction:
     def __init__(self, line):
         coords = Junction.parse(line)
-        self.c = [const.__MILS_TO_CM__ * x for x in coords]
+        self.c = [MILS_TO_CM * x for x in coords]
     
     def to_tek(self):
         return "({0},{1}) node[circ] {{}}\n".format(self.c[0],-self.c[1])
@@ -35,7 +37,7 @@ class Junction:
 class NoConnect:
     def __init__(self, line):
         coords = NoConnect.parse(line)
-        self.c = [const.__MILS_TO_CM__ * x for x in coords]
+        self.c = [MILS_TO_CM * x for x in coords]
     
     def to_tek(self):
         return "({0},{1}) node[ocirc] {{}}\n".format(self.c[0],-self.c[1])
@@ -55,19 +57,25 @@ class Component:
         self.block = text_block
         self.dict = {}
     def to_tek(self):
-        if self.dict["name"] in const.__BIPOLES__:
+        if self.dict["name"] in BIPOLES:
             # this component can be drawn as a bipole, the tek.lib is drawn so they can be rotated in the same manner
             # TODO I really don't like this 0.5
             
-            # TODO this if is here just as a temporary workaround
-            if self.dict["name"] == const.__NMOS__:
-                self.dict["name"] = "Tnmos"
-            if self.dict["name"] == const.__PMOS__:
-                self.dict["name"] = "Tpmos"
-            if self.dict["name"] == const.__NPN__:
-                self.dict["name"] = "Tnpn"
-            if self.dict["name"] == const.__PNP__:
-                self.dict["name"] = "Tpnp"
+            # TODO this if is here just as a temporary workaround, better use a dictionary.
+            # if self.dict["name"] == NMOS:
+                # self.dict["name"] = "Tnmos"
+                # H_STEP = 0.8
+            # elif self.dict["name"] == PMOS:
+                # self.dict["name"] = "Tpmos"
+                # H_STEP = 0.8
+            # elif self.dict["name"] == NPN:
+                # self.dict["name"] = "Tnpn"
+                # H_STEP = 0.8
+            # elif self.dict["name"] == PNP:
+                # self.dict["name"] = "Tpnp"
+                # H_STEP = 0.8
+            # else:
+                # H_STEP = 0.5
             
             x_start = x_end = self.dict["x"]
             y_start = y_end = self.dict["y"]
@@ -75,37 +83,37 @@ class Component:
             
             if self.dict["B"] == 1:
                 # left to right
-                x_start -= const.__H_STEP__
-                x_end += const.__H_STEP__
+                x_start -= H_STEP
+                x_end += H_STEP
                 if self.dict["C"] == -1:
                     mirror = True
             elif self.dict["B"] == -1:
                 # right to left
-                x_start += const.__H_STEP__
-                x_end -= const.__H_STEP__
+                x_start += H_STEP
+                x_end -= H_STEP
                 if self.dict["C"] == 1:
                     mirror = True
             elif self.dict["D"] == 1:
                 # up to down
-                y_start += const.__H_STEP__
-                y_end -= const.__H_STEP__
+                y_start += H_STEP
+                y_end -= H_STEP
                 if self.dict["A"] == 1:
                     mirror = True
             elif self.dict["D"] == -1:
                 # down to up
-                y_start -= const.__H_STEP__
-                y_end += const.__H_STEP__
+                y_start -= H_STEP
+                y_end += H_STEP
                 if self.dict["A"] == -1:
                     mirror = True
             if mirror:
-                return "({0},{1}) to [{5}, l=${2}$, mirror] ({3},{4})\n".format(x_start, y_start, self.dict["reference"], x_end, y_end, self.dict["name"])
+                return "({0},{1}) to [{5}, l=${2}$, mirror] ({3},{4})\n".format(x_start, y_start, self.dict["reference"], x_end, y_end, TRANSLATE[self.dict["name"]])
             else:
-                return "({0},{1}) to [{5}, l=${2}$] ({3},{4})\n".format(x_start, y_start, self.dict["reference"], x_end, y_end, self.dict["name"])
-        elif self.dict["name"] == const.__GND__:
+                return "({0},{1}) to [{5}, l=${2}$] ({3},{4})\n".format(x_start, y_start, self.dict["reference"], x_end, y_end, TRANSLATE[self.dict["name"]])
+        elif self.dict["name"] == GND:
             # Assuming ground is towards down
             # TODO must handle proper orientation!
             return "({0},{1}) node[ground]{{}}".format(self.dict["x"], self.dict["y"])
-        # elif self.dict["name"] == const.__NMOS__ or self.dict["name"] == const.__PMOS__:
+        # elif self.dict["name"] == NMOS or self.dict["name"] == PMOS:
             ## this component is a MOS transistor
             # print "mos found!"
             # return "({0},{1}) node[{2}]{{}}".format(self.dict["x"], self.dict["y"], self.dict["name"].lower())
@@ -117,29 +125,29 @@ class Component:
     def parse(self):
         block_iterator = iter(self.block.splitlines())
         for line in block_iterator:
-            while const.__END_COMPONENT__ not in line:
+            while END_COMPONENT not in line:
                 line = block_iterator.next()
-                if line.startswith(const.__COMP_LABEL__,0,len(const.__COMP_LABEL__)):
+                if line.startswith(COMP_LABEL,0,len(COMP_LABEL)):
                     temp = line.strip().split(" ")
                     temp.pop(0)
                     self.dict["name"] = temp.pop(0)
                     self.dict["reference"] = temp.pop(0)
-                elif line.startswith(const.__COMP_TIME__,0,len(const.__COMP_TIME__)):
+                elif line.startswith(COMP_TIME,0,len(COMP_TIME)):
                     temp = line.strip().split(" ")
                     temp.pop(0)
                     temp.pop(0)
                     temp.pop(0)  # Don't really know what the last two values are for
                     self.dict["time_stamp"] = temp.pop(0)
-                elif line.startswith(const.__COMP_POS__,0,len(const.__COMP_POS__)):
+                elif line.startswith(COMP_POS,0,len(COMP_POS)):
                     # Position line. WARNING: we must change y sign since the coordinates systems are different.
                     temp = line.strip().split(" ")
                     temp.pop(0)
-                    self.dict["x"] = int(temp.pop(0)) * const.__MILS_TO_CM__
-                    self.dict["y"] = -int(temp.pop(0)) * const.__MILS_TO_CM__
-                elif line.startswith(const.__COMP_FIELD__,0,len(const.__COMP_FIELD__)):
+                    self.dict["x"] = int(temp.pop(0)) * MILS_TO_CM
+                    self.dict["y"] = -int(temp.pop(0)) * MILS_TO_CM
+                elif line.startswith(COMP_FIELD,0,len(COMP_FIELD)):
                     # Need to think about this
                     do_something=1
-                elif line.startswith(const.__COMP_ENDING__,0,len(const.__COMP_ENDING__)):
+                elif line.startswith(COMP_ENDING,0,len(COMP_ENDING)):
                     # This line can be either the discontinued position line or the orientation matrix line
                     temp = line.strip().split()
                     if len(temp) == 4:
--- main.py
+++ main.py
@@ -4,20 +4,22 @@
 #author: Vladimir Cravero - vladimircravero@gmail.com
 
 # Generic imports
+#!/usr/bin/env python
+
 import sys
 
 # This file contains all the supported devices classes
 from devices import *
 
 # This file contains all the constants needed around
-import const
+from const import *
 
 """Begin of main program"""
 
-print "This is schToTek v{0}\nOpening files...".format(const.__VERSION__)
+print "This is schToTek v{0}\nOpening files...".format(VERSION)
 # Opening the files:
 try:
-    f_in = open(const.__INPUT_FILE_NAME__, "r")
+    f_in = open(INPUT_FILE_NAME, "r")
 except IOError as e:
     print "IO Error opening the input file({0}): {1}\n".format(e.errno, e.strerror)
     sys.exit(-1)
@@ -26,7 +28,7 @@ except:
     sys.exit(-1)
 
 try:
-    f_out = open(const.__OUT_FILE_NAME__, "w")
+    f_out = open(OUT_FILE_NAME, "w")
 except IOError as e:
     print "IO Error opening the output file({0}): {1}\n".format(e.errno, e.strerror)
     sys.exit(-1)
@@ -35,7 +37,7 @@ except:
     sys.exit(-1)
     
 try:
-    f_header = open(const.__TEK_HEADER_FILE_NAME__, "r")
+    f_header = open(TEK_HEADER_FILE_NAME, "r")
 except IOError as e:
     print "IO Error opening the input file({0}): {1}\n".format(e.errno, e.strerror)
     sys.exit(-1)
@@ -59,20 +61,20 @@ junctions_count = 0
 # Parser
 for line in f_in:
     lines_parsed += 1
-    if const.__WIRE__ in line:
+    if WIRE in line:
         line = f_in.next()
         lines_parsed += 1
         wires_count +=1
         f_out.write(Wire(line).to_tek())
-    elif const.__JUNCTION__ in line:
+    elif JUNCTION in line:
         f_out.write(Junction(line).to_tek())
         junctions_count += 1
-    elif const.__NO_CONNECT__ in line:
+    elif NO_CONNECT in line:
         f_out.write(NoConnect(line).to_tek())
-    elif const.__COMPONENT__ in line:
+    elif COMPONENT in line:
         components_count += 1
         component_block = ""
-        while const.__END_COMPONENT__ not in line:
+        while END_COMPONENT not in line:
             component_block = component_block + line
             line = f_in.next()
             lines_parsed += 1
@@ -82,7 +84,7 @@ for line in f_in:
         f_out.write(c.to_tek())
            
 #appending the footer            
-f_out.write(const.__TEK_FOOTER__)
+f_out.write(TEK_FOOTER)
 
 print "Done. Closing files..." 
 
